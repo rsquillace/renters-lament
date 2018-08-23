2604d3e60a788fb9d7093cb8942b99c1175625aa
Rachel Squillace <rmsquillace@gmail.com>
2018-08-22 18:31:57 -0700

    add agg affordability df and code

---
+++ src/merge_wage_rent.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+def transform_rent(rent_df):
+    trans_rent = []
+    for year in range(2011,2018):
+        year_df = rent_df[rent_df['year']==year].filter(items=['zipcode', 'med_rent']).copy()
+        transposed = year_df.astype(int).T.reset_index(drop=True).copy()
+        transposed.columns = transposed.iloc[0].astype(str)
+        transposed = transposed.drop([0])
+        transposed['year'] = year
+        trans_rent.append(transposed)
+    return pd.concat(trans_rent)
+
+def merge_rent_wage(rent_df, wage_df):
+    rent_trans = transform_rent(rent_df)
+    merged =  pd.merge(wage_df, rent_trans, on='year', how='left')
+    for col in merged.loc[:, merged.columns.str.startswith('98')].columns:
+        merged[col] = merged[col] <= merged['monthly_rent_allowance']
+    return merged

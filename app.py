8eff69d526e6005db3c16de19beef1ed086421ea
Rachel Squillace <rmsquillace@gmail.com>
2018-08-23 21:38:22 -0700

    prettifying web app

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def index():
 @app.route('/table/<industry_name>/<year>')
 def table(industry_name, year):
     zips = retrieve_affordable_zips(merged, industry_name, int(year))
-    n = len(zips)
-    x = range(n)
-    y = zips
-    return render_template('table.html', data=zip(x, y))
+    #n = len(zips)
+    x = zips
+    #y = zips
+    return render_template('table.html', data=x)
 
 def retrieve_affordable_zips(merged, industry_name, year):
     aff = merged.loc[:, merged.columns.str.startswith('98')].loc[(merged['industry'] == industry_name)&(merged['year'] == year)].values[0]

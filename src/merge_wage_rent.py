87d669199591494a9fff4e4192beb3786e163f74
Rachel Squillace <rmsquillace@gmail.com>
2018-08-26 14:37:45 -0700

    fixed merge method for diff df format

--- src/merge_wage_rent.py
+++ src/merge_wage_rent.py
@@ -2,15 +2,12 @@ import pandas as pd
 import numpy as np
 
 def transform_rent(rent_df):
-    trans_rent = []
-    for year in range(2011,2018):
-        year_df = rent_df[rent_df['year']==year].filter(items=['zipcode', 'med_rent']).copy()
-        transposed = year_df.astype(int).T.reset_index(drop=True).copy()
-        transposed.columns = transposed.iloc[0].astype(str)
-        transposed = transposed.drop([0])
-        transposed['year'] = year
-        trans_rent.append(transposed)
-    return pd.concat(trans_rent)
+    rent_df['zipcode'] = rent_df['zipcode'].astype(str).str.cat(rent_df['neighborhood'], sep=': ')
+    rent_df = rent_df.drop(columns=['neighborhood'])
+    transposed = rent_df.set_index('zipcode').T.reset_index().copy()
+    transposed = transposed.rename(columns={'index':'year'})
+    transposed['year'] = transposed['year'].astype(int)
+    return transposed
 
 def merge_rent_wage(rent_df, wage_df):
     rent_trans = transform_rent(rent_df)

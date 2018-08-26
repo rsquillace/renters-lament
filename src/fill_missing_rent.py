4d736a34daa20b25072efe96d35ec7116389f266
Rachel Squillace <rmsquillace@gmail.com>
2018-08-25 23:25:45 -0700

    change fill in methodology

--- src/fill_missing_rent.py
+++ src/fill_missing_rent.py
@@ -1,23 +1,31 @@
 import pandas as pd
 import numpy as np
 
-def create_normalization_dic(sale_df, year):
-    year_sales = sale_df[sale_df['year'] == year].copy()
-    normalized_dic = {}
-    zipcodes = sale_df['zipcode'].unique()
-    for zc in zipcodes:
-         normalized_dic[zc] = year_sales['med_sale_price'][year_sales['zipcode'] == zc].item()/year_sales['med_sale_price'][year_sales['zipcode']==98122].item()
-    return normalized_dic
+def make_rolling_mean_column(sale_df):
+    rolling_df = []
+    for zc in sale_df['zipcode'].unique():
+        zip_df = sale_df[sale_df['zipcode'] == zc].copy()
+        zip_df['rolling_mean'] = zip_df['med_sale_price'].rolling(2, min_periods=1).median()
+        rolling_df.append(zip_df)
+    return pd.concat([*rolling_df])
 
-def fill_in_values(rent_df, sale_df):
-    filled_in_dfs = []
-    for year in range(2011,2018):
-        year_rent = rent_df[rent_df['year'] == year].copy()
-        normalized_dic = create_normalization_dic(sale_df, year)
-        rent98122 = year_rent['med_rent'][year_rent['zipcode']==98122].item()
-        est_rent = {}
-        for k,v in normalized_dic.items():
-            est_rent[k] = v * rent98122
-        year_rent['med_rent'] = year_rent['med_rent'].fillna(year_rent['zipcode'].map(est_rent))
-        filled_in_dfs.append(year_rent)
-    return pd.concat(filled_in_dfs)
+def make_normalized_dic(sale_df, rent_df, zipcode):
+    sales = make_rolling_mean_column(sale_df)
+    sale_zip = sales[sales['zipcode'] == zipcode].copy()
+    non_null = rent_df.columns[rent_df[rent_df['zipcode'] == zipcode].notnull().any()].tolist()
+    earliest_known_year = min(non_null)
+    sale_zip['norm'] = sale_zip['rolling_mean'] / sale_zip[sale_zip['year'] == int(earliest_known_year)]['rolling_mean'].item()
+    dic = {}
+    for year in sale_zip['year'].unique():
+        dic[str(year)] = sale_zip[sale_zip['year'] == year]['norm'].item()
+    for k,v in dic.items():
+        dic[k] = v * rent_df[rent_df['zipcode'] == zipcode][earliest_known_year].item()
+    return dic
+
+def fill_rent(sale_df, rent_df):
+    for zc in rent_df['zipcode'].unique():
+        norm_dic = make_normalized_dic(sale_df, rent_df, zc)
+        null = rent_df.columns[rent_df.isnull().any()].tolist()
+        for year in null:
+            rent_df.at[rent_df['zipcode'] == zc, year] = norm_dic[year]
+    return rent_df

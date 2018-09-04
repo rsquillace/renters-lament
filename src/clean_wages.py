2e70498ad3e7cd21d6821fc3bf91321e4b053ac0
Rachel Squillace <rmsquillace@gmail.com>
2018-09-03 18:29:41 -0700

    remove industries with avg wage 0

--- src/clean_wages.py
+++ src/clean_wages.py
@@ -6,8 +6,8 @@ def clean_wage(data_file_path,data_year):
     wages = wages.dropna(subset=[wages.columns[1]])
     clean_wages = wages[wages.columns[[2, 18]]].copy()
     clean_wages = clean_wages.rename(columns={clean_wages.columns[0]:'industry', clean_wages.columns[1]:'avg_wage'})
-    clean_wages = clean_wages[clean_wages['avg_wage'] != '*'].reset_index(drop=True)
-    clean_wages[['avg_wage']] = clean_wages[['avg_wage']].astype(int)
+    clean_wages = clean_wages[(clean_wages['avg_wage'] != '*')&(clean_wages['avg_wage'] != 0)].reset_index(drop=True)
+    clean_wages['avg_wage'] = clean_wages['avg_wage'].astype(int)
     clean_wages['monthly_rent_allowance'] = (clean_wages['avg_wage'] / 12)/3
     clean_wages['year'] = data_year
     return clean_wages
@@ -28,7 +28,7 @@ def make_agg_wage_dataframe():
         dataframes.append(clean_wage(f'data/king_county_wages_{year}.xls', year))
     dataframes.extend((make_min_wage_df(10, 10.5, 11, 'Min Wage for Small Business w/ Bene or Tips'),
                       make_min_wage_df(11, 12, 13, 'Min Wage for Small Business'),
-                      make_min_wage_df(11, 12.5, 13.5, 'Min Wage for Lage Business w/ Bene'),
+                      make_min_wage_df(11, 12.5, 13.5, 'Min Wage for Large Business w/ Bene'),
                       make_min_wage_df(11, 13, 15, 'Min Wage for Large Business'))
                      )
     aggregated_wages = pd.concat(dataframes, sort=False).reset_index(drop=True)

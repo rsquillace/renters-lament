8de05fd24d51142695f0758f661f8430f94d97bc
Rachel Squillace <rmsquillace@gmail.com>
2018-09-07 17:15:51 -0700

    clean git repository

--- src/merge_wage_rent.py
+++ src/merge_wage_rent.py
@@ -16,5 +16,5 @@ def merge(one_bed_rent_df, two_bed_rent_df, wage_df):
     two_bed_merge['bedrooms'] = 2
     two_bed_merge['affordable'] = (two_bed_merge['med_rent'] / 2) <= two_bed_merge['monthly_rent_allowance']
     merged = pd.concat([one_bed_merge, two_bed_merge]).reset_index(drop=True)
-    merged['zipcode'] = merged['zipcode'].astype(str)
+    merged['zipcode'] = merged['zipcode'].astype(int)
     return merged
